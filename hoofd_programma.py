4cba4b476e98a072c537c189340925742bb09f58
Jonas Rammelaere <jonasrammelaere@gmail.com>
2023-12-07 06:55:05 +0100

    huiswerk les 10, aanmaak van boekhouding.py met bijhorende helper en presentatie functies

---
+++ hoofd_programma.py
@@ -0,0 +1,17 @@
+from helper import onderstreep
+
+uitvoer = onderstreep("AANBIEDING")
+uitvoer.append("Aardbeienijs, emmertje van 5 liter: 5 euro")
+uitvoer.append("Slagroom, spuitbus van 1 liter: 2 euro")
+print()
+
+for i in range(len(uitvoer)):
+    print(uitvoer[i])
+    
+'''
+#Alternatief: 
+
+for el in uitvoer:
+    print(el)
+
+'''
---
+++ presentatie.py
@@ -0,0 +1,5 @@
+def presenteer(dict, totaal):
+    for k, v in dict.items():
+        print(f"{k} : {v} euro")
+    print("="*25)
+    print(f"totaal : {totaal} euro")
